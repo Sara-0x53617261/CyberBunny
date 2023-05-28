cc9c5610e9c5aba8326cd71b00339b94a884e1ff
Sara-0x53617261 <sara98.sain@gmail.com>
2023-05-28 15:30:54 +0200

    Adding reports feature

--- src/commands/report.rs
+++ src/commands/report.rs
@@ -1,5 +1,22 @@
-use crate::{Context, Error};
-use serenity::builder::{CreateInputText, CreateActionRow};
+use crate::{Context, Error, Data};
+use poise::Modal;
+use tracing::info;
+
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
+
+type ApplicationContext<'a> = poise::ApplicationContext<'a, Data, Error>;
+
+#[derive(Debug, Modal)]
+#[name = "Bug Info"]
+struct BugInfoModal {
+    #[name = "Bug information"]
+    #[placeholder = "[bug info here]"]
+    #[paragraph]
+    #[min_length = 5]
+    #[max_length = 1000]
+    bug_info: String,
+}
 
 /// report parent command
 #[poise::command(slash_command, subcommands("bug",))]
@@ -8,22 +25,34 @@ pub async fn report(_ctx: Context<'_>) -> Result<(), Error> {
 }
 
 #[poise::command(slash_command)]
-pub async fn bug(ctx: Context<'_>) -> Result<(), Error> {
+pub async fn bug(ctx: ApplicationContext<'_>) -> Result<(), Error> {
+    let usern = format!("{}#{}", ctx.author().name, ctx.author().discriminator);
+    let uuid = ctx.id();
 
-    /*ctx.send(|e| {
-        e.components(|c| {
-            c.create_action_row(|r| {
-                r.create_input_text(|i| {
-                    i.custom_id("bug_description")
-                    .label("Bug Description")
-                    .required(true)
-                    .style(poise::serenity_prelude::InputTextStyle::Paragraph)
-                })
-            })
-        })
-    }).await?;*/
-    ctx.say("TODO").await?;
+    let report =  BugInfoModal::execute(ctx).await?;
+    info!("Got bug report from {}\n{:?}", usern, report);
 
+    // Todo: Find better way to save bug reports
 
+    if report.is_some() {
+            ctx.send(|msg| {
+                msg.content("Thank you, your bug report was received")
+                .ephemeral(true)
+            }).await?;
+
+            save_bug_report(report.unwrap(), uuid).await?;
+    } else {
+            ctx.send(|msg| {
+                msg.content("Looks like something went wrong, please let me know directly")
+                .ephemeral(true)
+            }).await?;
+    }
+
+    Ok(())
+}
+
+async fn save_bug_report(report: BugInfoModal, uuid: u64) -> Result<(), Error> {
+    let mut file = File::create(format!("bug_reports/{uuid}.txt")).await?;
+    file.write_all(report.bug_info.as_bytes()).await?;
     Ok(())
 }
